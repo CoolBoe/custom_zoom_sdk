62d9130eba9007083308d65591d1cbb1787d73e2
sagar <sagar@phoeniixx.com>
2021-08-25 10:54:46 +0530

    miner changes

--- example/lib/start_meeting_screen.dart
+++ example/lib/start_meeting_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:custom_zoom_sdk/custom_zoom_options.dart';
 import 'package:custom_zoom_sdk/custom_zoom_view.dart';
+import 'package:custom_zoom_sdk_example/constants.dart';
 
 import 'package:flutter/material.dart';
 
@@ -17,15 +18,15 @@ class StartMeetingWidget extends StatelessWidget {
   StartMeetingWidget({Key? key, meetingId}) : super(key: key) {
     this.zoomOptions = new CustomZoomOptions(
         domain: "zoom.us",
-        appKey: "0hOLo5oUgjNtcy0keTNDc6hfoNumJs2eebOk",
-        appSecret: "RLqYcfqrzkWp3Xm4TW1isJpybnVv9683yH0q",
+        appKey: Constants.APPKEY,
+        appSecret:Constants.APPSECRECT,
     );
     this.meetingOptions = new CustomZoomMeetingOptions(
-        userId: 'prashantinagdeve@gmail.com',
-        displayName: 'Rahul Sharma ',
-        meetingId: "89482898887",
-        zoomToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ5LWE5MEk3alFSYTlqdzNDYkphZlNRIiwiZXhwIjoxNjI5ODY1OTA2fQ.SFBddK9T2X_fMkVGtQfuLp_vVzZfjZ4qaeAUib2gCt8",
-        zoomAccessToken: "W7-buFfTKvNoCC47DjeGjxfAwMTqFZqomunRsdDxBTo.BgYsM0M0K2JaVm9ub0syTjlSSjNFU2RRc0w0ckk5Q0FXS21qZndzMVZQSmVDST1AMmE2ZDI3NDZjM2FkYWM2NGY5NTMwMzBiYzVhMzQxNTAxOWMwNTRiZmM1YjY2NTI1ZmQ4NDQ4MTAwMmE3NTA4ZgAgMGZBeEY1YVV2ODZoZE4vR2N2eUhGZTZ1SEJCQjlRV0cAAAAAAXt7kyfAABJ1AAAA",
+        userId: Constants.USERID,
+        displayName: Constants.DISPLAYNAME,
+        meetingId: Constants.MEETINGID,
+        zoomToken: Constants.USERTOKEN,
+        zoomAccessToken: Constants.ACCESSTOKEN,
         disableDialIn: "true",
         disableDrive: "true",
         disableInvite: "true",
